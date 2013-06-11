3a5be58b6a71fecb3f56b2c322f0b20f9343bd08
lgoldstien <lgoldstien@onmylemon.co.uk>
2013-06-11 14:03:22 -0700

    Generate the TestTaker Entity

---
+++ src/OML/TypingTestBundle/Entity/TestTaker.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace OML\TypingTestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TestTaker
+ */
+class TestTaker
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $fullname;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fullname
+     *
+     * @param string $fullname
+     * @return TestTaker
+     */
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+    
+        return $this;
+    }
+
+    /**
+     * Get fullname
+     *
+     * @return string 
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return TestTaker
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
